4a0b90b5d7e94eafe19a7f7c063affbb743e3766
Titan <Titan@debian-titan.debian-titan>
2025-11-27 08:26:27 +0530

    created add , description & setTime variables

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ func main(){
 	fmt.Println("this is task tracker")
 	//define a flag
 	nameFlag:= flag.String("name","World","Your name")
-	add := flag.String("add"
+
+	add := flag.String("add-task","new task","Add your task name")
+	description := flag.String("description","description","add description")
+	setTime := flag.String("set-time","1****","set time in cron expression")
 	//parse the flags
 	flag.Parse()
 	//use the value
